8afd9007ba141cbaa65df75939704b122ee23c51
kyoshidajp <claddvd@gmail.com>
2016-10-31 22:21:56 +0900

    Implement finder class methods

--- num_finder.rb
+++ num_finder.rb
@@ -1,5 +1,35 @@
 class NumFinder
-  def find
 
+  MIN_NUMBER = 1
+  MAX_NUMBER = 1000
+
+  def initialize
+    @numbers = Array(MIN_NUMBER..MAX_NUMBER).shuffle
   end
+
+  def take
+    @selected_number ||= @numbers.delete_at(rand(@numbers.length))
+  end
+
+  def find
+    return @find_number unless @find_number.nil?
+    total = @numbers.inject(:+)
+    max = @numbers.length + 1
+    @find_number = Array(1..max).inject(:+) - total
+  end
+
+  def debug
+    print <<-EOS
+      numbers: #{@numbers}
+      selected_number: #{@selected_number}
+      find_number: #{@find_number}
+    EOS
+  end
+end
+
+if __FILE__ == $0
+  finder = NumFinder.new
+  finder.take
+  finder.find
+  finder.debug
 end
--- num_finder_spec.rb
+++ num_finder_spec.rb
@@ -3,12 +3,37 @@ require_relative 'num_finder'
 
 describe NumFinder do
 
+  describe '.initialize' do
+
+    it 'should array is shuffled' do
+      finder1 = NumFinder.new
+      finder2 = NumFinder.new
+      sorted_array = Array(NumFinder::MIN_NUMBER..NumFinder::MAX_NUMBER)
+      expect(finder1.instance_variable_get(:@numbers)).not_to eq sorted_array
+      expect(finder2.instance_variable_get(:@numbers)).not_to eq sorted_array
+    end
+  end
+
   describe '.find' do
 
     context 'when random array is set manually as [1, 5, 2, 4]' do
       it 'returns 3' do
         finder = NumFinder.new
+        finder.instance_eval do
+          @numbers = [1, 5, 2, 4]
+        end
         expect(finder.find).to eq 3
+      end
+    end
+
+    context 'when random array is set automatically' do
+      it 'returns taken number' do
+        finder = NumFinder.new
+        finder.take
+        finder.find
+        selected_number = finder.instance_variable_get(:@selected_number)
+        numbers = finder.instance_variable_get(:@numbers)
+        expect(numbers.find {|n| n == selected_number}).to be_nil
       end
     end
   end
