76a29bad7e0187d17ecfa6e7e17ef9fd635fcd63
kyoshidajp <claddvd@gmail.com>
2016-11-03 00:18:25 +0900

    Add check the found number is in the range

--- num_finder_spec.rb
+++ num_finder_spec.rb
@@ -16,17 +16,6 @@ describe NumFinder do
 
   describe '.find' do
 
-    context 'when random array is set manually as [1, 5, 2, 4]' do
-      it 'returns 3' do
-        finder = NumFinder.new
-        finder.instance_eval do
-          NumFinder::MAX_NUMBER = 5
-          @numbers = [1, 5, 2, 4]
-        end
-        expect(finder.find).to eq 3
-      end
-    end
-
     context 'when random array is set automatically' do
       it 'returns taken number' do
         finder = NumFinder.new
@@ -35,6 +24,8 @@ describe NumFinder do
         found_number = finder.instance_variable_get(:@found_number)
         numbers = finder.instance_variable_get(:@numbers)
         expect(numbers.find {|n| n == found_number}).to be_nil
+        expect(found_number).to be >= NumFinder::MIN_NUMBER
+        expect(found_number).to be <= NumFinder::MAX_NUMBER
       end
     end
   end
@@ -43,10 +34,11 @@ describe NumFinder do
     it 'returns taken number' do
       finder = NumFinder.new
       finder.pick_up
-      finder.send(:find_by_subtraction)
-      found_number = finder.instance_variable_get(:@found_number)
+      found_number = finder.send(:find_by_subtraction)
       numbers = finder.instance_variable_get(:@numbers)
       expect(numbers.find {|n| n == found_number}).to be_nil
+      expect(found_number).to be >= NumFinder::MIN_NUMBER
+      expect(found_number).to be <= NumFinder::MAX_NUMBER
     end
   end
 
@@ -54,10 +46,11 @@ describe NumFinder do
     it 'returns taken number' do
       finder = NumFinder.new
       finder.pick_up
-      finder.send(:find_by_array_sub)
-      found_number = finder.instance_variable_get(:@found_number)
+      found_number = finder.send(:find_by_array_sub)
       numbers = finder.instance_variable_get(:@numbers)
       expect(numbers.find {|n| n == found_number}).to be_nil
+      expect(found_number).to be >= NumFinder::MIN_NUMBER
+      expect(found_number).to be <= NumFinder::MAX_NUMBER
     end
   end
 
@@ -65,10 +58,11 @@ describe NumFinder do
     it 'returns taken number' do
       finder = NumFinder.new
       finder.pick_up
-      finder.send(:find_by_each_compare)
-      found_number = finder.instance_variable_get(:@found_number)
+      found_number = finder.send(:find_by_each_compare)
       numbers = finder.instance_variable_get(:@numbers)
       expect(numbers.find {|n| n == found_number}).to be_nil
+      expect(found_number).to be >= NumFinder::MIN_NUMBER
+      expect(found_number).to be <= NumFinder::MAX_NUMBER
     end
   end
 end
