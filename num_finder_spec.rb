85428c8ae974cc73ea7d7d70c96395d745bdd56f
kyoshidajp <claddvd@gmail.com>
2016-11-03 09:03:32 +0900

    Refactor spec by shared_examples

--- num_finder_spec.rb
+++ num_finder_spec.rb
@@ -3,6 +3,16 @@ require_relative 'num_finder'
 
 describe NumFinder do
 
+  shared_examples 'found number' do
+    it 'is in the range' do
+      expect(found_number).to be >= NumFinder::MIN_NUMBER
+      expect(found_number).to be <= NumFinder::MAX_NUMBER
+    end
+    it 'returns picked up number' do
+      expect(numbers.find {|n| n == found_number}).to be_nil
+    end
+  end
+
   describe '.initialize' do
 
     it 'should array is shuffled' do
@@ -12,7 +22,7 @@ describe NumFinder do
       numbers2 = finder2.instance_variable_get(:@numbers)
       sorted_array = Array(NumFinder::MIN_NUMBER..NumFinder::MAX_NUMBER)
 
-      # carefully check
+      # check carefully
       not_same_order1 = sorted_array != numbers1
       not_same_order2 = sorted_array != numbers2
       expect(not_same_order1 && not_same_order2).to eq true
@@ -33,64 +43,44 @@ describe NumFinder do
   describe '.find' do
 
     context 'when random array is set automatically' do
-      it 'returns taken number' do
-        finder = NumFinder.new
-        finder.pick_up
-        finder.find
-        found_number = finder.instance_variable_get(:@found_number)
-        numbers = finder.instance_variable_get(:@numbers)
-        expect(numbers.find {|n| n == found_number}).to be_nil
-        expect(found_number).to be >= NumFinder::MIN_NUMBER
-        expect(found_number).to be <= NumFinder::MAX_NUMBER
-      end
+      finder = NumFinder.new
+      finder.pick_up
+      finder.find
+      let(:numbers) { finder.instance_variable_get(:@numbers) }
+      let(:found_number) { finder.instance_variable_get(:@found_number) }
+      it_behaves_like 'found number'
     end
   end
 
   describe '.find_by_subtraction' do
-    it 'returns taken number' do
-      finder = NumFinder.new
-      finder.pick_up
-      found_number = finder.send(:find_by_subtraction)
-      numbers = finder.instance_variable_get(:@numbers)
-      expect(numbers.find {|n| n == found_number}).to be_nil
-      expect(found_number).to be >= NumFinder::MIN_NUMBER
-      expect(found_number).to be <= NumFinder::MAX_NUMBER
-    end
+    finder = NumFinder.new
+    finder.pick_up
+    let(:numbers) { finder.instance_variable_get(:@numbers) }
+    let(:found_number) { finder.send(:find_by_subtraction) }
+    it_behaves_like 'found number'
   end
 
   describe '.find_by_array_sub' do
-    it 'returns taken number' do
-      finder = NumFinder.new
-      finder.pick_up
-      found_number = finder.send(:find_by_array_sub)
-      numbers = finder.instance_variable_get(:@numbers)
-      expect(numbers.find {|n| n == found_number}).to be_nil
-      expect(found_number).to be >= NumFinder::MIN_NUMBER
-      expect(found_number).to be <= NumFinder::MAX_NUMBER
-    end
+    finder = NumFinder.new
+    finder.pick_up
+    let(:numbers) { finder.instance_variable_get(:@numbers) }
+    let(:found_number) { finder.send(:find_by_array_sub) }
+    it_behaves_like 'found number'
   end
 
   describe '.find_by_each_compare' do
-    it 'returns taken number' do
-      finder = NumFinder.new
-      finder.pick_up
-      found_number = finder.send(:find_by_each_compare)
-      numbers = finder.instance_variable_get(:@numbers)
-      expect(numbers.find {|n| n == found_number}).to be_nil
-      expect(found_number).to be >= NumFinder::MIN_NUMBER
-      expect(found_number).to be <= NumFinder::MAX_NUMBER
-    end
+    finder = NumFinder.new
+    finder.pick_up
+    let(:numbers) { finder.instance_variable_get(:@numbers) }
+    let(:found_number) { finder.send(:find_by_each_compare) }
+    it_behaves_like 'found number'
   end
 
   describe '.find_by_c' do
-    it 'returns taken number' do
-      finder = NumFinder.new
-      finder.pick_up
-      found_number = finder.send(:find_by_c)
-      numbers = finder.instance_variable_get(:@numbers)
-      expect(numbers.find {|n| n == found_number}).to be_nil
-      expect(found_number).to be >= NumFinder::MIN_NUMBER
-      expect(found_number).to be <= NumFinder::MAX_NUMBER
-    end
+    finder = NumFinder.new
+    finder.pick_up
+    let(:numbers) { finder.instance_variable_get(:@numbers) }
+    let(:found_number) { finder.send(:find_by_c) }
+    it_behaves_like 'found number'
   end
 end
